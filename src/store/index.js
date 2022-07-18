c13ed6bddbd6f411261c428d8c23523819335bdb
liujie <2894997560@qq.com>
2022-07-18 11:08:51 +0800

    整体布局代码优化

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,17 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    collapsed: false,
   },
   mutations: {
+    changeCollapsed(state) {
+      state.collapsed = !state.collapsed;
+    },
   },
   actions: {
+    changeCollapsed({ commit }) {
+      commit('changeCollapsed');
+    },
   },
-  modules: {
-  },
+  modules: {},
 });
